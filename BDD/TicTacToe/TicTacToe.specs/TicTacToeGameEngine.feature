337130375e5cbad2164fb94d4428fc6dd435dd31
James Bender <james@jamescbender.com>
2015-10-11 16:54:23 -0400

    added slides and demo

---
+++ BDD/TicTacToe/TicTacToe.specs/TicTacToeGameEngine.feature
@@ -0,0 +1,36 @@
+﻿Feature: Tic Tac Toe
+	In order to determine who the winner of the game as
+	As a Player
+	I want to be told who (if anyone) won the game
+
+Scenario: Empty board ends with a tie
+	Given I have a tic tac toe board
+	And the board is empty
+	When I determine the winner
+	Then the result be a tie
+
+Scenario: When X is accross the top then X wins
+	Given I have a tic tac toe board
+	And I have an 'X' in the top left
+	And I have an 'X' in the top middle 
+	And I have an 'X' in the top right
+	When I determine the winner
+	Then the result will be an 'X'
+
+Scenario: When O is accross the top then O wins
+	Given I have a tic tac toe board
+	And I have an 'O' in the top left
+	And I have an 'O' in the top middle
+	And I have an 'O' in the top right
+	When I determine the winner
+	Then the result will be an 'O'
+
+Scenario: When the board looks like this then someone wins
+	Given I have a tic tac toe board
+	And the board looks like this
+	| col1 | col2 | col3 |
+	| X    |      |      |
+	| O    | O    | X    |
+	| X    | O    |      |
+	When I determine the winner
+	Then the result be a tie
---
+++ BDD/TicTacToe/TicTacToe.specs/TicTacToeSteps.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+using TicTacToe.Core;
+
+namespace TicTacToe.specs
+{
+    [Binding]
+    public class TicTacToeSteps
+    {
+        private string[,] _board;
+        private string _result;
+        private GameEngine _gameEngine = new GameEngine();
+
+        [Given(@"I have a tic tac toe board")]
+        public void GivenIHaveATicTacToeBoard()
+        {
+            _board = new String[3, 3] ;// {{" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "}};
+        }
+        
+        [Given(@"the board is empty")]
+        public void GivenTheBoardIsEmpty()
+        {
+            _board = new String[3, 3] { { " ", " ", " " }, { " ", " ", " " }, { " ", " ", " " } };
+        }
+        
+        [When(@"I determine the winner")]
+        public void WhenIDetermineTheWinner()
+        {
+            _result = _gameEngine.GetWinner(_board);
+        }
+        
+        [Then(@"the result be a tie")]
+        public void ThenTheResultBeATie()
+        {
+            Assert.AreEqual(" ", _result);
+        }
+
+        [Given(@"I have an '(.*)' in the top left")]
+        public void GivenIHaveAnInTheTopLeft(string p0)
+        {
+            _board[0, 0] = p0;
+        }
+
+        [Given(@"I have an '(.*)' in the top middle")]
+        public void GivenIHaveAnInTheTopMiddle(string p0)
+        {
+            _board[0, 1] = p0;
+        }
+
+        [Given(@"I have an '(.*)' in the top right")]
+        public void GivenIHaveAnInTheTopRight(string p0)
+        {
+            _board[0, 2] = p0;
+        }
+
+        [Then(@"the result will be an '(.*)'")]
+        public void ThenTheResultWillBeAn(string p0)
+        {
+            Assert.AreEqual(p0, _result);
+        }
+
+        [Given(@"the board looks like this")]
+        public void GivenTheBoardLooksLikeThis(Table table)
+        {
+            _board[0, 0] = table.Rows[1]["col1"];
+        }
+
+
+    }
+}
